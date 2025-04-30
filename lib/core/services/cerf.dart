2a208c08d38a71818853c506b39a0e24f86d97c8
Thaiminh2022 <thaiminh2022@gmail.com>
2025-04-30 12:35:35 +0700

    files

---
+++ lib/core/services/cerf.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+import 'package:app_ta/core/models/result.dart';
+
+class CerfReader {
+  Future<Result<List<String>, String>> readCsv() async {
+    final file = File("");
+    return Result.err("Un implement");
+  }
+}
