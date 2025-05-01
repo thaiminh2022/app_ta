7c2c4d05261833e2c86d200855c293983b281251
Thaiminh2022 <thaiminh2022@gmail.com>
2025-05-01 15:55:46 +0700

    Feature hangman (#2)
    * hangman_init
    
    * remove unescess
    
    * quick touch
    
    * seperate files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app_ta/core/providers/app_state.dart';
 import 'package:app_ta/features/dictionary/presentation/index.dart';
+import 'package:app_ta/features/games/hangman/presentation/index.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -20,7 +21,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application
   @override
   void initState() {
     super.initState();
@@ -48,10 +48,7 @@ class BottomNavbar extends StatefulWidget {
 
 class _BottomNavbarState extends State<BottomNavbar> {
   var _idx = 0;
-  final List<Widget> _widgetOptions = <Widget>[
-    DictionarySearch(),
-    Text('Placeholder'),
-  ];
+  final List<Widget> _widgetOptions = <Widget>[DictionarySearch(), Hangman()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
