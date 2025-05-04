ffac25e34274f4747f43a62de88c8d93c94a28d6
quannguyen247 <nguyendongquan247@gmail.com>
2025-05-05 00:29:07 +0700

    Doi ten app

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DailyE',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue.shade600),
       ),
