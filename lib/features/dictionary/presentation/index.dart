95a0d09fd2b66f3f100a54d5d79b031b3c8a4ade
Thaiminh2022 <thaiminh2022@gmail.com>
2025-05-04 10:01:06 +0700

    use SearchController

--- lib/features/dictionary/presentation/index.dart
+++ lib/features/dictionary/presentation/index.dart
@@ -7,72 +7,88 @@ import 'package:flutter/material.dart';
 class DictionarySearch extends StatelessWidget {
   DictionarySearch({super.key});
 
-  final TextEditingController _controller = TextEditingController();
+  final SearchController _controller = SearchController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Dictionary")),
-      body: Container(
-        margin: EdgeInsets.all(20),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.library_books, size: 150),
-              SizedBox(height: 20),
-              TextField(
-                controller: _controller,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.library_books, size: 150),
+            SizedBox(height: 10),
+            DictionarySearchBarView(controller: _controller),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (_controller.text.isEmpty) return;
 
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Seach term",
-                  label: Text("search word"),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        if (_controller.text.isEmpty) return;
-
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder:
-                                (context) =>
-                                    WordInfoView(searchWord: _controller.text),
-                          ),
-                        );
-                      },
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(Icons.search),
-                          SizedBox(width: 10),
-                          Text("Search"),
-                        ],
-                      ),
-                    ),
-                  ),
-                  FilledButton(
-                    onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (ctx) => LearnedWordsView()),
+                        MaterialPageRoute(
+                          builder:
+                              (context) =>
+                                  WordInfoView(searchWord: _controller.text),
+                        ),
                       );
                     },
-                    child: Text("Learned words"),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.search),
+                        SizedBox(width: 10),
+                        Text("Search"),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+                FilledButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (ctx) => LearnedWordsView()),
+                    );
+                  },
+                  child: Text("Learned words"),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class DictionarySearchBarView extends StatelessWidget {
+  const DictionarySearchBarView({super.key, required this.controller});
+
+  final SearchController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return SearchAnchor(
+      searchController: controller,
+      suggestionsBuilder: (context, controller) {
+        return [];
+      },
+      builder: (context, controller) {
+        return SearchBar(
+          controller: controller,
+          hintText: "hello",
+          padding: const WidgetStatePropertyAll<EdgeInsets>(
+            EdgeInsets.symmetric(horizontal: 16),
+          ),
+
+          leading: Icon(Icons.search),
+        );
+      },
     );
   }
 }
