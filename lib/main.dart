111c043bd40fb2f4f06eea3d43c2c2422c66acc2
Khang <hatapuka@gmail.com>
2025-05-06 20:08:26 +0700

    Update main.dart
    Update lại theo structure folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'dart:io';
+
 import 'package:app_ta/core/providers/app_state.dart';
-import 'package:app_ta/features/custom_splash_screen.dart'; // Import CustomSplashScreen
+import 'package:app_ta/features/custom_splash_screen.dart';
 import 'package:app_ta/features/dictionary/presentation/index.dart';
-
-import 'package:app_ta/features/pronunciation_checker/presentation/index.dart';
 import 'package:app_ta/features/games/hangman/presentation/index.dart';
-
+import 'package:app_ta/features/dashboard/presentation/index.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-
+import 'package:app_ta/features/word_of_the_day/presentation/index.dart'; // Thêm màn hình Word of the Day
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 void main() {
   runApp(
     ChangeNotifierProvider(
@@ -51,12 +55,12 @@ class BottomNavbar extends StatefulWidget {
 }
 
 class _BottomNavbarState extends State<BottomNavbar> {
-  var _idx = 0;
-
-
+  var _idx = 2;
   final List<Widget> _widgetOptions = <Widget>[
+    WordOfTheDayScreen(),
     DictionarySearch(),
     PronunciationCheckerScreen(),
+    const Dashboard(),
     Hangman(),
   ];
 
@@ -69,8 +73,8 @@ class _BottomNavbarState extends State<BottomNavbar> {
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.book), label: "Dictionary"),
           BottomNavigationBarItem(
-            icon: Icon(Icons.mic),
-            label: "Pronunciation",
+            icon: Icon(Icons.dashboard),
+            label: "Dashboard",
           ),
           BottomNavigationBarItem(icon: Icon(Icons.gamepad), label: "Hangman"),
         ],
