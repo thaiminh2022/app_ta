743552b14be30605c1a65e34b44596277bc216a0
Thaiminh2022 <thaiminh2022@gmail.com>
2025-05-07 19:46:40 +0700

    weird update

--- Lib/features/word_of_the_day/models/word_of_the_day_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:app_ta/core/models/word_cerf.dart';
-
-class WordOfTheDayModel {
-  String word;
-  WordCerf cerf;
-  String? definition;
-
-  WordOfTheDayModel({required this.word, required this.cerf, this.definition});
-}
